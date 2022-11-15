62798745d86ee837a0c253faed1b47e7495a195a
Jayson Webster <jayson_webster27@hotmail.com>
2022-11-15 09:48:16 -0800

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,15 @@
+#Demonstration of For Loop, asks for a number and stores that number, counts up to 100, and counts the difference between the number entered and 100
+
+#Define Variablres
+numberstr = input("Please enter a number: ")
+numberint = int(numberstr)
+print(f"{numberint}, {numberstr}")        #Starting point
+
+for numberint in range(numberint, 101):  #As long as the number entered +count is less than or equal to 100, run this loop
+    print(f"int:{numberint}, str:{numberstr}"))
+    numberint += 1                       #increases numberint, so loop does not run infinitely
+
+numberint -= 1                           #Reduces numberint by 1, so it does not read 101
+print(f"Iterations: {iteration}")
+print(f"Original Number: {numberstr}")
+print(f"Final Number: {numberint}")
---
+++ includingmath2.py
@@ -0,0 +1,2 @@
+import math2
+#Import includes another python file, it will be executed the moment the import line is found.
